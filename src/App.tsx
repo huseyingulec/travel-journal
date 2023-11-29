3f2d5239a17d52a6f4f5fde1002f1291629cec0a
Huseyin Gulec <itshuseyingulec@gmail.com>
2023-11-29 23:32:46 +0100

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import data from './data.json'
+import './App.css'
+import Nav from './Nav'
+
+function App() {
+  return (
+    <Nav />
+  )
+}
+
+export default App
---
+++ src/Nav.tsx
@@ -0,0 +1,10 @@
+import "./App.css";
+
+export default function Nav() {
+    return (
+        <nav className="Nav">
+            <img className="nav--logo" src="./src/assets/worldLogo.svg" alt="World Logo" />
+            <h4 className="nav--title">Huey's Travel Journal</h4>
+        </nav>
+    );
+}
