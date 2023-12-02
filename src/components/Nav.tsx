6eae25ebf2078a8320cd11297646820eea573e4c
Huseyin Gulec <itshuseyingulec@gmail.com>
2023-12-02 02:26:02 +0100

    feat:add burger navbar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,14 +1,34 @@
 import "./App.scss";
+import logo from "../assets/worldLogo.svg";
+
 
 export default function Nav() {
     return (
         <nav>
-            <img
-                className="nav--logo"
-                src="./src/assets/worldLogo.svg"
-                alt="World Logo"
-            />
-            <h4 className="nav--title">Huey's Travel Journal</h4>
+            <div className="nav--title">
+                <img className="nav--logo" src={logo} alt="World Logo" />
+                <h4>Huey's Travel Journal</h4>  
+            </div>
+
+            {/* Burger icon */}
+            <input type="checkbox" id="burger-toggle" />
+            <label htmlFor="burger-toggle" id="burger" className="burger">
+                <span></span>
+                <span></span>
+                <span></span>
+            </label>
+
+            {/* Navbar menu */}
+            <div id="navbar--menu">
+                <ul>
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">About me</a>
+                    </li>
+                </ul>
+            </div>
         </nav>
     );
 }
