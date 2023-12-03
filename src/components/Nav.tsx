b38e574c147df6bf8bd17e1d9c66a29b52c8f7b7
Huseyin Gulec <itshuseyingulec@gmail.com>
2023-12-03 03:40:09 +0100

    feat: add functional navbar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,33 +1,35 @@
-import "./App.scss";
-import logo from "../assets/worldLogo.svg";
-
+import "./Nav.scss";
+import logo from "../assets/logo-travel.png";
 
 export default function Nav() {
     return (
         <nav>
-            <div className="nav--title">
+            {/* Navbar header */}
+            <a href="#home" className="nav--header">
                 <img className="nav--logo" src={logo} alt="World Logo" />
-                <h4>Huey's Travel Journal</h4>  
-            </div>
+                <h4 className="nav--title">Huey's Travel Journal</h4>
+            </a>
 
             {/* Burger icon */}
-            <input type="checkbox" id="burger-toggle" />
-            <label htmlFor="burger-toggle" id="burger" className="burger">
-                <span></span>
-                <span></span>
-                <span></span>
-            </label>
+            <div>
+                <input type="checkbox" id="burger-toggle" />
+                <label htmlFor="burger-toggle" id="burger" className="burger">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </label>
 
-            {/* Navbar menu */}
-            <div id="navbar--menu">
-                <ul>
-                    <li>
-                        <a href="#">Home</a>
-                    </li>
-                    <li>
-                        <a href="#">About me</a>
-                    </li>
-                </ul>
+                {/* Navbar menu */}
+                <div id="navbar--menu">
+                    <ul>
+                        <li>
+                            <a href="#home">Home</a>
+                        </li>
+                        <li>
+                            <a href="#aboutme">About me</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
     );
